02f781fad353c116c4cf504b9b8ece7aa713a41d
Taeung Song <treeze.taeung@gmail.com>
2015-07-16 18:28:49 +0900

    perf config: Add '--system' and '--user' options to select which config file is used
    Which config file is used is decided in only perf_config().
    And a perf-confg command depend on perf_config()
    getting config file path. So add '--system' and '--user' options
    to select which config file to be used without perf_config().
    If file-options isn't used, default config file path is
    $HOME/.perfconfig.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- tools/perf/builtin-config.c
+++ tools/perf/builtin-config.c
@@ -13,6 +13,9 @@
 #include "util/util.h"
 #include "util/debug.h"
 
+static bool use_system_config, use_user_config;
+static const char *config_file_name;
+
 static const char * const config_usage[] = {
 	"perf config [options]",
 	NULL
@@ -23,6 +26,9 @@ enum actions {
 } actions;
 
 static const struct option config_options[] = {
+	OPT_GROUP("Config file location"),
+	OPT_BOOLEAN(0, "system", &use_system_config, "use system config file"),
+	OPT_BOOLEAN(0, "user", &use_user_config, "use user config file"),
 	OPT_GROUP("Action"),
 	OPT_SET_UINT('l', "list", &actions,
 		     "show current config variables", ACTION_LIST),
@@ -47,15 +53,28 @@ int cmd_config(int argc, const char **argv, const char *prefix __maybe_unused)
 	argc = parse_options(argc, argv, config_options, config_usage,
 			     PARSE_OPT_STOP_AT_NON_OPTION);
 
+	if (use_system_config && use_user_config) {
+		pr_err("Error: only one config file at a time\n");
+		goto out_err;
+	}
+	config_file_name = mkpath("%s/.perfconfig", getenv("HOME"));
+	if (use_system_config || (!use_user_config &&
+				  access(config_file_name, R_OK) == -1))
+		config_file_name = perf_etc_perfconfig();
+
 	switch (actions) {
 	case ACTION_LIST:
 	default:
 		if (argc) {
 			pr_err("Error: unknown argument\n");
-			usage_with_options(config_usage, config_options);
+			goto out_err;
 		} else
-			ret = perf_config(show_config, NULL);
+			ret = perf_config_from_file(show_config, config_file_name, NULL);
 	}
 
 	return ret;
+
+out_err:
+	usage_with_options(config_usage, config_options);
+	return -1;
 }
