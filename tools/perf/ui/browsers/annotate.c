a4a7a746d454c6e24ffd680fb29576a866d29a4a
Taeung Song <treeze.taeung@gmail.com>
2016-05-09 20:00:35 +0900

    perf config: Initialize annotate_browser__opts with default config items
    Set default config values for 'annotate' section with 'annotate_config_items[]'
    instead of actual bool type values.
    (e.g. using annotate_config_items[CONFIG_ANNOTATE_USE_OFFSET].value
    instead of 'true' bool type value for 'annotate.use_offset'.)
    
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- tools/perf/ui/browsers/annotate.c
+++ tools/perf/ui/browsers/annotate.c
@@ -37,10 +37,7 @@ static struct annotate_browser_opt {
 	     show_linenr,
 	     show_nr_jumps,
 	     show_total_period;
-} annotate_browser__opts = {
-	.use_offset	= true,
-	.jump_arrows	= true,
-};
+} annotate_browser__opts;
 
 struct annotate_browser {
 	struct ui_browser b;
@@ -1160,5 +1157,12 @@ static int annotate__config(const char *var, const char *value,
 
 void annotate_browser__init(void)
 {
+	annotate_browser__opts.hide_src_code = CONF_ANNOTATE_DEFAULT_VAL(HIDE_SRC_CODE, b);
+	annotate_browser__opts.use_offset = CONF_ANNOTATE_DEFAULT_VAL(USE_OFFSET, b);
+	annotate_browser__opts.jump_arrows = CONF_ANNOTATE_DEFAULT_VAL(JUMP_ARROWS, b);
+	annotate_browser__opts.show_linenr = CONF_ANNOTATE_DEFAULT_VAL(SHOW_LINENR, b);
+	annotate_browser__opts.show_nr_jumps = CONF_ANNOTATE_DEFAULT_VAL(SHOW_NR_JUMPS, b);
+	annotate_browser__opts.show_total_period = CONF_ANNOTATE_DEFAULT_VAL(SHOW_TOTAL_PERIOD, b);
+
 	perf_config(annotate__config, NULL);
 }
