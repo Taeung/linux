0da363004926eb5d2dc0d2dcb3e769c31750d7bd
Taeung Song <treeze.taeung@gmail.com>
2016-05-31 01:06:39 +0900

    perf config: Reimplement perf_config() using perf_config_set__iter()
    Everytime perf_config() is called, perf_config() always read config files.
    (i.e. user config '~/.perfconfig' and system config '$(sysconfdir)/perfconfig')
    
    But we need to use the config set that already contains all config
    key-value pairs to avoid this repetitive work reading the config files
    in perf_config(). (the config set mean a global variable 'config_set')
    
    In other words, if new perf_config() is called,
    only first time 'config_set' is initialized collecting all configs
    from the config files and it work with perf_config_set__iter().
    And free the config set after sub-command work at run_builtin().
    
    If we do, 'config_set' can be reused wherever using perf_config()
    and a feature of old perf_config() is the same as new perf_config() work
    without the repetitive work that read the config files.
    
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Wang Nan <wangnan0@huawei.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -391,6 +391,7 @@ static int run_builtin(struct cmd_struct *p, int argc, const char **argv)
 
 	perf_env__set_cmdline(&perf_env, argc, argv);
 	status = p->fn(argc, argv, prefix);
+	perf_config_set__delete();
 	exit_browser(status);
 	perf_env__exit(&perf_env);
 	bpf__clear();
