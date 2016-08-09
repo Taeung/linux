14788bad1ed7f558fe68c3f65a9a824a2046ef3f
Taeung Song <treeze.taeung@gmail.com>
2016-08-09 13:43:52 +0900

    perf config: Add default section and item arrays for 'annotate' config
    Actual variable for configs of 'annotate' section is like below.
    
    (at ui/browsers/annoate.c)
    static struct annotate_browser_opt {
            bool hide_src_code,
                 use_offset,
                 jump_arrows,
                 show_linenr,
                 show_nr_jumps,
                 show_total_period;
    } annotate_browser__opts = {
            .use_offset      = true,
            .jump_arrows     = true,
    };
    
    But I suggest using default config arrays for 'annotate' section that
    contain all default config key-value pairs for it.
    
    In near future, this arrays will be used on ui/browsers/annoate.c
    because of setting default values of actual variables for 'annotate' config.
    
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- tools/perf/util/config.h
+++ tools/perf/util/config.h
@@ -75,6 +75,7 @@ enum perf_config_type {
 
 enum config_section_idx {
 	CONFIG_COLORS,
+	CONFIG_ANNOTATE,
 };
 
 enum colors_config_items_idx {
@@ -85,6 +86,15 @@ enum colors_config_items_idx {
 	CONFIG_COLORS_JUMP_ARROWS,
 	CONFIG_COLORS_ADDR,
 	CONFIG_COLORS_ROOT,
+};
+
+enum annotate_config_items_idx {
+	CONFIG_ANNOTATE_HIDE_SRC_CODE,
+	CONFIG_ANNOTATE_USE_OFFSET,
+	CONFIG_ANNOTATE_JUMP_ARROWS,
+	CONFIG_ANNOTATE_SHOW_NR_JUMPS,
+	CONFIG_ANNOTATE_SHOW_LINENR,
+	CONFIG_ANNOTATE_SHOW_TOTAL_PERIOD,
 };
 
 struct default_config_item {
@@ -128,5 +138,6 @@ struct default_config_section {
 
 extern const struct default_config_section default_sections[];
 extern const struct default_config_item colors_config_items[];
+extern const struct default_config_item annotate_config_items[];
 
 #endif /* __PERF_CONFIG_H */
