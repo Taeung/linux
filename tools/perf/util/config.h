4bc5c30243a38a31ea771986653042cae66ae1c0
Taeung Song <treeze.taeung@gmail.com>
2016-08-09 13:43:43 +0900

    perf config: Add macros assigning key-value pairs to default_config_item
    In near future, default_config_item arrays will be added
    (e.g. const struct default_config_item colors_config_items[])
    To simply assign config key-value pairs to default_config_item,
    add macros that are CONF_VAR() and CONF_*_VAR() for each type.
    
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Wang Nan <wangnan0@huawei.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- tools/perf/util/config.h
+++ tools/perf/util/config.h
@@ -92,4 +92,24 @@ struct default_config_section {
 	const struct default_config_item *items;
 };
 
+#define CONF_VAR(_name, _field, _val, _type)			\
+	{ .name = _name, .value._field = _val, .type = _type }
+
+#define CONF_BOOL_VAR(_name, _val)			\
+	CONF_VAR(_name, b, _val, CONFIG_TYPE_BOOL)
+#define CONF_INT_VAR(_name, _val)			\
+	CONF_VAR(_name, i, _val, CONFIG_TYPE_INT)
+#define CONF_LONG_VAR(_name, _val)			\
+	CONF_VAR(_name, l, _val, CONFIG_TYPE_LONG)
+#define CONF_U64_VAR(_name, _val)			\
+	CONF_VAR(_name, ll, _val, CONFIG_TYPE_U64)
+#define CONF_FLOAT_VAR(_name, _val)			\
+	CONF_VAR(_name, f, _val, CONFIG_TYPE_FLOAT)
+#define CONF_DOUBLE_VAR(_name, _val)			\
+	CONF_VAR(_name, d, _val, CONFIG_TYPE_DOUBLE)
+#define CONF_STR_VAR(_name, _val)			\
+	CONF_VAR(_name, s, _val, CONFIG_TYPE_STRING)
+#define CONF_END()					\
+	{ .name = NULL }
+
 #endif /* __PERF_CONFIG_H */
